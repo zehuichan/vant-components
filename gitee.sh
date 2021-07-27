69c007998b66c58041be14a40009bdf80016fd65
jasconchan <jasonchenzehui@gmail.com>
2021-07-27 22:57:24 +0800

    support github gh-pages

---
+++ gitee.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env sh
+
+# 终止一个错误
+set -e
+
+# 构建
+npm run build:docs
+
+# 进入生成的构建文件夹
+cd docs
+
+git init
+git add -A
+git commit -m 'deploy'
+
+# 如果你想要部署到 https://<USERNAME>.github.io
+git push https://gitee.com/zehuichan/vcomponents.git master
+
+cd -
