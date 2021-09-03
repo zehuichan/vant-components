35987f6c1fe0d1cde8cbf6755de6a9cdfcdbf268
chenzehui <jasonchenzehui@gmail.com>
2021-09-03 17:50:37 +0800

    feat: 新增vautocomplete组件

---
+++ examples/views/vautocomplete/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <demo-wrapper title="VAutoComplete 自动填充">
+    <demo-card>
+      <v-auto-complete label="switch" placeholder="placeholder"/>
+    </demo-card>
+    <README/>
+  </demo-wrapper>
+</template>
+
+<script>
+import README from './README.md'
+
+export default {
+  name: 'vswitch',
+  data() {
+    return {
+      value: 1
+    }
+  },
+  components: {
+    README
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
