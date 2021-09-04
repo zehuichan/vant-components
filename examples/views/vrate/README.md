5c88ff61dd1a6d77154da32548903bf57b64256a
jasconchan <jasonchenzehui@gmail.com>
2021-09-04 23:18:02 +0800

    feat: 新增表单项组件 - 评分

---
+++ examples/views/vrate/README.md
@@ -0,0 +1,19 @@
+### 介绍
+
+表单项类型 - 评分。
+
+### 基础用法
+
+```html
+ <v-rate label="rate" v-model="value" />
+```
+
+```js
+export default {
+  data() {
+    return {
+      value: 1
+    }
+  },
+}
+```
---
+++ examples/views/vrate/index.vue
@@ -0,0 +1,46 @@
+<template>
+  <demo-wrapper title="VRate 评分">
+    <demo-card title="基础用法">
+      <v-rate label="rate" v-model="value"/>
+    </demo-card>
+    <demo-card title="自定义图标">
+      <v-rate label="rate" v-model="value" icon="like" void-icon="like-o"/>
+    </demo-card>
+    <demo-card title="自定义样式">
+      <v-rate label="rate" v-model="value" color="#ffd21e" void-icon="star" void-color="#eee"/>
+    </demo-card>
+    <demo-card title="半星">
+      <v-rate label="rate" v-model="value" allow-half void-icon="star" void-color="#eee"/>
+    </demo-card>
+    <demo-card title="自定义数量">
+      <v-rate label="rate" v-model="value" :count="6"/>
+    </demo-card>
+    <demo-card title="禁用状态">
+      <v-rate label="rate" v-model="value" disabled/>
+    </demo-card>
+    <demo-card title="只读状态">
+      <v-rate label="rate" v-model="value" readonly/>
+    </demo-card>
+    <README/>
+  </demo-wrapper>
+</template>
+
+<script>
+import README from './README.md'
+
+export default {
+  name: 'vrate',
+  data() {
+    return {
+      value: 2.5
+    }
+  },
+  components: {
+    README
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
