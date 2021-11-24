62a33de1f4109dfcdd6e77fc29482bf8f40b4f4d
chenzehui <jasonchenzehui@gmail.com>
2021-11-24 17:16:46 +0800

    feat: 新增属性选择器

--- examples/views/vchecker/README.md
+++ examples/views/vchecker/README.md
@@ -1,27 +1,90 @@
-### 介绍
-
-提供多个选项集合供用户选择，仅支持单列选择，多列选择，级联选择。
-
-必须按照vant的数据格式食用。
-
 ### 基础用法
 
 ```html
 <!--basic-->
-<v-picker v-model="value" :columns="columns" label="picker" placeholder="placeholder" clearable/>
+<v-checker
+  class="demo-checker"
+  v-model="value"
+  title="标题"
+  :columns="columns"
+/>
+<!--分组-->
+<v-checker
+  class="demo-checker"
+  v-model="value2"
+  title="标题"
+  :columns="columns2"
+  @confirm="onConfirm"
+  @cancel="onCancel"
+/>
+<!--搭配单元格使用-->
+<van-cell title="setValue" :border="false" is-link @click="value2 = [ 11, 12, 13, 14, 15 ]"/>
+<van-cell title="resetValue" :border="false" is-link @click="value2 = []"/>
+<van-cell title="属性选择" :value="value2.toString()" :border="false" is-link @click="show = true"/>
+<van-popup v-model="show" :round="false" position="bottom" get-container="body" safe-area-inset-bottom>
+  <v-checker
+    v-model="value2"
+    title="标题"
+    :columns="columns2"
+    multiple
+    @confirm="onConfirm"
+    @cancel="onCancel"
+  />
+</van-popup>
 ```
 
 ```js
 export default {
   data() {
     return {
-      value: [],
+      show: false,
+      value: [1],
+      value2: [11, 12, 13],
       columns: [
         { label: '上海', value: 1 },
         { label: '北京', value: 2, disabled: true },
         { label: '广州', value: 3 },
         { label: '深圳', value: 4 },
+      ],
+      columns2: [
+        {
+          title: '分组1',
+          values: [
+            { label: '周一', value: 11 },
+            { label: '周二', value: 12 },
+            { label: '周三', value: 13 },
+            { label: '周四', value: 14 },
+            { label: '周五', value: 15 },
+          ]
+        },
+        {
+          title: '分组2',
+          values: [
+            { label: '上午', value: 21, imgUrl: 'https://b.yzcdn.cn/vant/sku/shoes-1.png' },
+            { label: '下午', value: 22, imgUrl: 'https://b.yzcdn.cn/vant/sku/shoes-2.png' },
+            { label: '晚上', value: 23, imgUrl: 'https://b.yzcdn.cn/vant/sku/shoes-3.png' },
+          ]
+        },
+        {
+          title: '分组3',
+          disabled: true,
+          values: [
+            { label: '吃饭', value: 31 },
+            { label: '睡觉', value: 32 },
+            { label: '打游戏', value: 33 },
+          ]
+        },
       ]
+    }
+  },
+  methods: {
+    onConfirm() {
+      this.show = false
+      this.$toast('confirm: ' + this.value2.toString())
+    },
+    onCancel() {
+      this.show = false
+      this.$toast('cancel')
     }
   },
 }
@@ -34,9 +97,9 @@ export default {
 | 参数   | 说明           | 类型      | 默认值 |
 | ------ | -------------- | --------- | ------ |
 | v-model (value) | 绑定值 | _array_  | _    |
-| options | 对象数组，配置每一列显示的数据 | _Options[]_  | `[]`    |
-| label   | 左侧标题   | _string \| number_  | _    |
-| placeholder   | 输入框占位提示文字     | _string_  | _    |
-| disabled     | 是否禁用     | _boolean_  | `false`    |
-| readonly     | 是否只读     | _boolean_  | `false`    |
+| title | 标题 | _string_  | _    |
+| columns | 对象数组，配置每一列显示的数据 | _Options[]_  | `[]`    |
+| multiple | 是否多选 | _boolean_  | `false`  |
+| multiple-limit | 多选时用户最多可以选择的项目数，为 0 则不限制 | _number_  | `0`    |
+| lazy-load | 是否开启图片懒加载 | _boolean_  | `false`  |
 | prop | 唯一标识的键名 | _object_  | `{ label: 'label', value: 'value' }`    |
--- examples/views/vchecker/index.vue
+++ examples/views/vchecker/index.vue
@@ -1,35 +1,37 @@
 <template>
-  <demo-wrapper title="VChecker 多选选择器">
+  <demo-wrapper title="VChecker 选择器">
     <demo-card title="基础用法">
-      <div class="nstration">默认为 Date 对象: {{ value }}</div>
+      <div class="nstration">单选: {{ value }}</div>
       <v-checker
+        class="demo-checker"
         v-model="value"
+        title="标题"
         :columns="columns"
       />
     </demo-card>
     <demo-card title="分组">
-      <div class="nstration">默认为 Date 对象: {{ value2 }}</div>
+      <div class="nstration">多选: {{ value2 }}</div>
       <v-checker
+        class="demo-checker"
         v-model="value2"
+        title="标题"
         :columns="columns2"
-        multiple
-        :multiple-limit="5"
+        @confirm="onConfirm"
+        @cancel="onCancel"
       />
     </demo-card>
-    <demo-card title="搭配表单项使用">
-      <van-field
-        label="属性选择"
-        :value="value2.toString()"
-        readonly
-        clickable
-        @click="show = true"
-      />
-      <van-popup v-model="show" round position="bottom">
+    <demo-card title="搭配单元格使用">
+      <van-cell title="setValue" :border="false" is-link @click="value2 = [ 11, 12, 13, 14, 15 ]"/>
+      <van-cell title="resetValue" :border="false" is-link @click="value2 = []"/>
+      <van-cell title="属性选择" :value="value2.toString()" :border="false" is-link @click="show = true"/>
+      <van-popup v-model="show" :round="false" position="bottom" get-container="body" safe-area-inset-bottom>
         <v-checker
           v-model="value2"
+          title="标题"
           :columns="columns2"
           multiple
-          :multiple-limit="5"
+          @confirm="onConfirm"
+          @cancel="onCancel"
         />
       </van-popup>
     </demo-card>
@@ -45,8 +47,8 @@ export default {
   data() {
     return {
       show: false,
-      value: [],
-      value2: [],
+      value: [1],
+      value2: [11, 12, 13],
       columns: [
         { label: '上海', value: 1 },
         { label: '北京', value: 2, disabled: true },
@@ -55,8 +57,7 @@ export default {
       ],
       columns2: [
         {
-          title: 'title1',
-          disabled: true,
+          title: '分组1',
           values: [
             { label: '周一', value: 11 },
             { label: '周二', value: 12 },
@@ -66,7 +67,7 @@ export default {
           ]
         },
         {
-          title: 'title2',
+          title: '分组2',
           values: [
             { label: '上午', value: 21, imgUrl: 'https://b.yzcdn.cn/vant/sku/shoes-1.png' },
             { label: '下午', value: 22, imgUrl: 'https://b.yzcdn.cn/vant/sku/shoes-2.png' },
@@ -74,14 +75,25 @@ export default {
           ]
         },
         {
-          title: 'title3',
+          title: '分组3',
+          disabled: true,
           values: [
             { label: '吃饭', value: 31 },
             { label: '睡觉', value: 32 },
             { label: '打游戏', value: 33 },
           ]
         },
-      ],
+      ]
+    }
+  },
+  methods: {
+    onConfirm() {
+      this.show = false
+      this.$toast('confirm: ' + this.value2.toString())
+    },
+    onCancel() {
+      this.show = false
+      this.$toast('cancel')
     }
   },
   components: {
@@ -89,3 +101,9 @@ export default {
   }
 }
 </script>
+
+<style>
+.demo-checker {
+  width: 375px;
+}
+</style>
