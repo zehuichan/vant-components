dc84037358af0f86fad17deae715a22860ebc916
zehuichan <jasonchenzehui@gmail.com>
2022-02-23 16:34:14 +0800

    feat: f2图表支持数据重绘

--- examples/views/vf2/index.vue
+++ examples/views/vf2/index.vue
@@ -1,8 +1,19 @@
 <template>
   <demo-wrapper title="Antv/F2 可视化">
     <demo-card>
-      <v-f2 :init="basic"/>
+      <van-tabs v-model="active" type="card" style="width: 400px;" @click="onAction">
+        <van-tab title="日"/>
+        <van-tab title="月"/>
+        <van-tab title="年"/>
+      </van-tabs>
+      <div style="padding: 8px 8px 0;">
+        <v-f2 :init="basic" :data="chartData"/>
+      </div>
+    </demo-card>
+    <demo-card>
       <v-f2 :init="line"/>
+    </demo-card>
+    <demo-card>
       <v-f2 :init="area"/>
     </demo-card>
     <README/>
@@ -10,13 +21,64 @@
 </template>
 
 <script>
-import { basic, line, area } from './examples'
+import { line, area } from './examples'
 import README from './README.md'
+
+function random(x, y) {
+  return Math.round(Math.random() * (y - x) + x)
+}
 
 export default {
   name: 'vf2',
+  data() {
+    return {
+      active: 0,
+      chartData: []
+    }
+  },
+  created() {
+    this.chartData = [
+      { genre: 'Sports', sold: 275 },
+      { genre: 'Strategy', sold: 115 },
+      { genre: 'Action', sold: 120 },
+      { genre: 'Shooter', sold: 350 },
+      { genre: 'Other', sold: 150 },
+    ]
+  },
   methods: {
-    basic,
+    onAction(name, title) {
+      if (name === 0) {
+        this.chartData = [
+          { genre: 'Sports', sold: random(100, 200) },
+          { genre: 'Strategy', sold: random(100, 200) },
+          { genre: 'Action', sold: random(100, 200) },
+          { genre: 'Shooter', sold: random(100, 200) },
+          { genre: 'Other', sold: random(100, 200) },
+        ]
+      }
+      if (name === 1) {
+        this.chartData = [
+          { genre: 'Sports', sold: random(100, 200) },
+          { genre: 'Strategy', sold: random(100, 200) },
+          { genre: 'Action', sold: random(100, 200) },
+          { genre: 'Shooter', sold: random(100, 200) },
+          { genre: 'Other', sold: random(100, 200) },
+        ]
+      }
+      if (name === 2) {
+        this.chartData = []
+      }
+    },
+    basic(F2, options) {
+      const chart = new F2.Chart(options)
+
+      chart.source(this.chartData)
+      chart.interval().position('genre*sold').color('genre')
+
+      chart.render()
+
+      return chart
+    },
     line,
     area
   },
